80802e749321723c38f702e12e1ea0e6c17ed970
sunit sarkar <sunit6503@gmail.com>
2024-09-17 20:17:58 +0530

    linkediin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a target="blank" href="https://github.com/sunitsarkar"><img src={navIcon4} alt="Icon" /></a>
-              <a target="blank" href="https://www.linkedin.com/in/srkr99/"><img src={navIcon1} alt="Icon" /></a>
+              <a target="blank" href="https://www.linkedin.com/in/sunitskr/"><img src={navIcon1} alt="Icon" /></a>
               <a target="blank" href="https://www.facebook.com/profile.php?id=100090071738445&mibextid=ZbWKwL"><img src={navIcon2} alt="Icon" /></a>
               <a target="blank" href="https://www.instagram.com/sunitskr?igsh=cGMzb2twcmszZHEz"><img src={navIcon3} alt="Icon" /></a>
             </div>
